045988abd537a9548315e53462c07e50dc29a1c3
Thomas L'Anglais <tlang072@uottawa.ca>
2016-09-16 09:10:02 -0400

    changing the GeometricSequenceTester slightly

--- Lab 1/Lab 1/src/GeometricSequenceTester.java
+++ Lab 1/Lab 1/src/GeometricSequenceTester.java
@@ -1,13 +1,14 @@
 public class GeometricSequenceTester {
 	public static void main(String[] args) {
 		GeometricSequence seq = new GeometricSequence(2,4);
-		for(int i = 0; i < 6; i++){
+		for (int i = 0; i < 6; i++){
 			System.out.print(seq.get_element_by_index(i) + " ");
 		}
 		System.out.println("Sum: " + seq.get_sum_of_elements(0, 6));
 
-		GeometricSequence seq2 = new GeometricSequence(-5,5);
-		for(int i = 0; i < 10; i++){
+		GeometricSequence seq2 = new GeometricSequence(-2,3);
+
+		for (int i = 5; i < 10; i++){
 			System.out.print(seq2.get_element_by_index(i) + " ");
 		}
 		System.out.println("Sum: " + seq2.get_sum_of_elements(5, 10));
