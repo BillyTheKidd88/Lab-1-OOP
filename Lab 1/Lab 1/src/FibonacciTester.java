9ab71e34d543c105a81022b6b953de2570cf48af
BillyTheKidd88 <patn97@gmail.com>
2016-09-19 12:43:24 -0400

    Add files via upload

--- Lab 1/Lab 1/src/FibonacciTester.java
+++ Lab 1/Lab 1/src/FibonacciTester.java
@@ -4,7 +4,6 @@ public class FibonacciTester {
 	public static void main(String[] args){
 		final int []ref_Values = {0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,4181,6765};
 		Random rnd = new Random();
-
 		FibonacciSequence seq = new FibonacciSequence();
 		for (int i=0; i<20; i++){
 			int next = seq.get_next_element();
@@ -12,7 +11,7 @@ public class FibonacciTester {
 				System.out.println("get_next_element Error F" + (i+1) + " is supposed to be " + ref_Values[i+1] + " but " + next+ " was calculated instead!");
 			}
 			if (seq.get_index() != i+1) {
-				System.out.println("get_index Error1 index value retrieved " + seq.get_index() + " is different from expected " + i+1);
+				System.out.println("get_index Error1 index value retrieved " + seq.get_index() + " is different from expected " + i+1);            
 			}
 		}
 		for (int i=20; i>0; i--){
@@ -21,25 +20,15 @@ public class FibonacciTester {
 				System.out.println("Error F" + (i-1) + " is supposed to be " + ref_Values[i-1] + " but " + prev+ " was calculated instead!");
 			}
 			if (seq.get_index() != i-1) {
-				System.out.println("get_index Error2 index value retrieved " + seq.get_index() + " is different from expected " + i+1);
+				System.out.println("get_index Error2 index value retrieved " + seq.get_index() + " is different from expected " + i+1);            
 			}
 		}
 		for (int i=0; i<5; i++){
 			int ii = rnd.nextInt(20);
 			int elem = seq.get_element_by_index(ii);
 			if (elem != ref_Values[ii]){
-				System.out.println("get_element_by_index Error F" + (ii) + " is supposed to be " + ref_Values[ii] + " but " + elem + " was calculated instead!");
+				System.out.println("get_element_by_index Error F" + (ii) + " is supposed to be " + ref_Values[ii] + " but " + elem + " was calculated instead!");            
 			}
 		}
-		for (int i = 0; i < 5; i++) {
-			System.out.println("Fib(" + i + ") = " + seq.get_element_by_index(i));
-		}
-		System.out.println("Sum(0-5) = " + seq.get_sum_of_elements(0,5));
-		System.out.println();
-		for (int i = 4; i < 11; i++) {
-			System.out.println("Fib(" + i + ") = " + seq.get_element_by_index(i));
-		}
-		System.out.println("Sum(4-11) = " + seq.get_sum_of_elements(4,11));
-		System.out.println();
 	}
 }
